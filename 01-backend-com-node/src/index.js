0aaecb0b045d55411bf6e723abc29d6e5b0da2fb
Matheus B. Grigoletto <matheus.grigoletto@me.com>
2020-10-02 22:33:48 -0300

    Nível 01 / 01 Backend com node / Métodos HTTP

--- 01-backend-com-node/src/index.js
+++ 01-backend-com-node/src/index.js
@@ -4,11 +4,58 @@ const express = require("express");
 // inicializa o express
 const app = express();
 
-// rota raiz com HTTP GET
-app.get("/", (request, response) => {
+/**
+ * Métodos HTTP:
+ *
+ * GET: buscar informações (recursos) do back-end
+ * POST: criar uma informação no back-end
+ * PUT/PATCH: alterar uma informação no back-end
+ * DELETE: excluir uma informação no back-end
+ */
+
+// HTTP GET
+app.get("/projects", (request, response) => {
   // retornando um json
   return response.json({
-    message: "Hello world!",
+    data: ["Projeto Ambar", "Projeto Blackbird", "Projeto Codecanyon"],
+  });
+});
+
+// HTTP POST
+app.post("/projects", (request, response) => {
+  // lógica para tratar e salvar dados enviados ...
+
+  // retornar alguma informação
+  return response.json({
+    data: [
+      "Projeto Ambar",
+      "Projeto Blackbird",
+      "Projeto Codecanyon",
+      "Projeto Destinybond",
+    ],
+  });
+});
+
+// HTTP PUT
+app.put("/projects/:id", (request, response) => {
+  // lógica para tratar e salvar (atualizar) os dados enviados ...
+
+  return response.json({
+    data: [
+      "Projeto Ambar",
+      "Projeto Blackbird",
+      "Projeto CleanCyan",
+      "Projeto Destinybond",
+    ],
+  });
+});
+
+// HTTP DELETE
+app.delete("/projects/:id", (request, response) => {
+  // lógica para tratar excluir os dados enviados ...
+
+  return response.json({
+    data: ["Projeto Ambar", "Projeto Blackbird", "Projeto CleanCyan"],
   });
 });
 

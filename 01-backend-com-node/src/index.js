ea24321185ac0a71bc6fd7becca6e87606329869
Matheus B. Grigoletto <matheus.grigoletto@me.com>
2020-10-02 22:56:28 -0300

    Nível 01 / 01 Backend com node / Tipos de Parâmetros

--- 01-backend-com-node/src/index.js
+++ 01-backend-com-node/src/index.js
@@ -1,8 +1,15 @@
 // imports
 const express = require("express");
 
+// => Atenção: as intruções devem ser definidas/executadas na ordem certa,
+//    de cima pra baixo
+
 // inicializa o express
 const app = express();
+
+// use() => middlewares
+// configurando para receber json no body
+app.use(express.json());
 
 /**
  * Métodos HTTP:
@@ -13,17 +20,32 @@ const app = express();
  * DELETE: excluir uma informação no back-end
  */
 
+/**
+ * Tipos de parâmetros:
+ *
+ * Query params: mais usado em filtros e paginação
+ * Route params: identificar recursos na hora de atualizar ou excluir
+ * Request body: conteúdo na hora de criar ou editar um recurso, normalmente
+ *    vindo via JSON
+ */
+
 // HTTP GET
 app.get("/projects", (request, response) => {
+  const { title, owner } = request.query;
+
   // retornando um json
   return response.json({
     data: ["Projeto Ambar", "Projeto Blackbird", "Projeto Codecanyon"],
+    title,
+    owner,
   });
 });
 
 // HTTP POST
 app.post("/projects", (request, response) => {
-  // lógica para tratar e salvar dados enviados ...
+  const { title, owner } = request.body;
+
+  // => lógica para tratar e salvar dados enviados ...
 
   // retornar alguma informação
   return response.json({
@@ -33,12 +55,16 @@ app.post("/projects", (request, response) => {
       "Projeto Codecanyon",
       "Projeto Destinybond",
     ],
+    title,
+    owner,
   });
 });
 
 // HTTP PUT
 app.put("/projects/:id", (request, response) => {
-  // lógica para tratar e salvar (atualizar) os dados enviados ...
+  const { id } = request.params;
+
+  // => lógica para tratar e salvar (atualizar) os dados enviados ...
 
   return response.json({
     data: [
@@ -47,12 +73,15 @@ app.put("/projects/:id", (request, response) => {
       "Projeto CleanCyan",
       "Projeto Destinybond",
     ],
+    id,
   });
 });
 
 // HTTP DELETE
 app.delete("/projects/:id", (request, response) => {
-  // lógica para tratar excluir os dados enviados ...
+  const { id } = request.params;
+
+  // => lógica para tratar excluir os dados enviados ...
 
   return response.json({
     data: ["Projeto Ambar", "Projeto Blackbird", "Projeto CleanCyan"],

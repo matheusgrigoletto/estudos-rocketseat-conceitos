34d1a90fa5787be88a055fbbf20b6ce13f7ca830
Matheus B. Grigoletto <matheus.grigoletto@me.com>
2020-10-17 14:47:48 -0300

    Nível 02 / Primeiro projeto com Node.js / Model de Agendamento

--- 02-01-primeiro-projeto-node/src/routes/appointments.routes.ts
+++ 02-01-primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
 
 const appointmentsRouter: Router = Router();
-
-interface Appointment {
-  id: string;
-  provider: string;
-  date: Date;
-}
 
 const appointments: Appointment[] = [];
 
@@ -27,11 +21,7 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked.' });
   }
 
-  const appointment = {
-    id: v4(),
-    provider,
-    date: parsedDate,
-  };
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
 
